8472f4bc61762ff613337324bfe2138bb50bdd01
Aoun Naqvi <18321519-173@uog.edu.pk>
2022-04-24 08:46:42 +0500

    Commit: randomaly generated colors using state

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,14 +1,23 @@
 import React , {useState} from 'react';
-import {Text , View, Button, StyleSheet } from 'react-native';
+import {Text , View, Button, StyleSheet, FlatList } from 'react-native';
+
 const Colorgenerator = () => {
   
-    const [colors , setColors]=useState("");
+    const [colors , setColors]=useState([]);
 
     return( <View >
-        <Button  title='Add Color' />
-        <View style={{height:100,width:100, backgroundColor: random_color()}}/>
-
-    </View>
+        <Button  title='Add Color' onPress={() =>{
+//creating a new array by doing [], (...colors) will copy all the data of colors into new array, new random color will be added by random_Color
+            setColors([...colors, random_color()]); 
+        }}/>
+       <FlatList  
+       keyExtractor={(item) => item}
+       data={colors}
+        renderItem={({item}) =>{
+       return <View style={{height:100,width:100, backgroundColor: item}}/>
+       }} 
+        />
+    </View>  
 
     );
 };
