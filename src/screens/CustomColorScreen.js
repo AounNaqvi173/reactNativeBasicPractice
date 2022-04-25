3768ce839e0db7dd158d6118e157be3255823eb1
Aoun Naqvi <18321519-173@uog.edu.pk>
2022-04-25 10:55:25 +0500

    mapping the functionality

--- src/screens/CustomColorScreen.js
+++ src/screens/CustomColorScreen.js
@@ -13,26 +13,50 @@ const CustomColor = () => {
  const [ green , setGreen]=useState(0);
 
  const COLOR_INCREMENT= 15 ;
-                                                /* If a child needs to change a state value , the parent can 
+ const setcolor=(color,change)=>{
+     //color === 'red', 'green', 'blue'
+     //change === +15, -15
+ 
+     switch(color) {
+         case 'red':
+             if(red+change >255 || red + change <0){
+                    return;  }
+            else {setRed(red +change);}
+                break;
+        case 'blue':
+            if(blue+change >255 || blue + change <0){
+                    return;  }
+            else {setBlue(blue +change);}
+                 break;    
+        case 'green':    
+             if(green+change >255 || green + change <0){
+                     return;  }
+            else {setGreen(green +change);}
+    default:
+                 return;  
+    }
+
+ };
+                                                 /* If a child needs to change a state value , the parent can 
                                                 pass down a callback function to change the state value as a prop.
                                                 As in this case we need our child components that is Colorcomponent 
                                                 to change the three different state values of red, bue and green */
 
 
     return( <View>
-     <Colorcomponent  onIncrease={ () => { setRed(red +  COLOR_INCREMENT)} } 
-        onDecrease={ () => { setRed(red -  COLOR_INCREMENT)}}
+     <Colorcomponent  onIncrease={ () => { setcolor('red', COLOR_INCREMENT)} } 
+        onDecrease={ () => { setcolor('red' , -1 * COLOR_INCREMENT)}}
         title="Red"  
         />
         
-        <Colorcomponent onIncrease={() => { setBlue(blue +  COLOR_INCREMENT)}} 
-          onDecrease={() => {setBlue(blue -  COLOR_INCREMENT)}} 
+        <Colorcomponent onIncrease={() => { setcolor('blue' ,  COLOR_INCREMENT)}} 
+          onDecrease={() => {setcolor('blue' , -1 * COLOR_INCREMENT)}} 
           title="Blue" 
          />
         
-        <Colorcomponent onIncrease={() => { setGreen(green + COLOR_INCREMENT)}} 
-         onDecrease={() => {setGreen(green - COLOR_INCREMENT)}} 
-             title="Green" 
+        <Colorcomponent onIncrease={() => { setcolor('green' , COLOR_INCREMENT)}} 
+         onDecrease={() => {setcolor('green' ,-1 * COLOR_INCREMENT)}} 
+             title="Green"  
          />
 
          <View style={{ height:160 , width: 160 , backgroundColor: `rgb(${red}, ${green} , ${blue})`}} />
